7cb60385b4270ae3381b5a7b1ff67bedf8929b92
Shanika Perera <shanikachp@gmail.com>
2024-06-12 23:28:25 -0500

    get embeddings using vertexai

--- tasks/task_4/task_4.py
+++ tasks/task_4/task_4.py
@@ -1,5 +1,6 @@
 # embedding_client.py
 
+import streamlit as st
 from langchain_google_vertexai import VertexAIEmbeddings
 
 class EmbeddingClient:
@@ -30,11 +31,13 @@ class EmbeddingClient:
     """
     
     def __init__(self, model_name, project, location):
-        # Initialize the VertexAIEmbeddings client with the given parameters
+        # Initialize the VertexAIEmbeddings client with the parameters
         # Read about the VertexAIEmbeddings wrapper from Langchain here
         # https://python.langchain.com/docs/integrations/text_embedding/google_generative_ai
         self.client = VertexAIEmbeddings(
-            #### YOUR CODE HERE ####
+            model_name = model_name,
+            project = project,
+            location = location
         )
         
     def embed_query(self, query):
@@ -62,11 +65,15 @@ class EmbeddingClient:
 
 if __name__ == "__main__":
     model_name = "textembedding-gecko@003"
-    project = "YOUR PROJECT ID HERE"
+    project = "platinum-tube-425318-d5"
     location = "us-central1"
 
     embedding_client = EmbeddingClient(model_name, project, location)
-    vectors = embedding_client.embed_query("Hello World!")
+    query = "Hello World!"
+    vectors = embedding_client.embed_query(query)
     if vectors:
-        print(vectors)
+        #print(vectors)
         print("Successfully used the embedding client!")
+        
+        st.write(f"Embeddings for {query}")
+        st.write(vectors)
